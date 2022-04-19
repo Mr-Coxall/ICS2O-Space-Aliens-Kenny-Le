b8ae97e84a8d9e6623d03a67fb00c34b01c5f39f
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-19 16:44:28 +0000

    added comments to our files

--- js/game.js
+++ js/game.js
@@ -1,14 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Kenny Le All rights reserved
+//
 // Created by: Kenny Le
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
