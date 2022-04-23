38ff3014f03d27c4e32452f833ad58c0ff5a7964
Patrick Coxall <mr.coxall@mths.ca>
2022-04-23 12:49:44 -0400

    add in jsdoc comments for methods

--- js/splashScene.js
+++ js/splashScene.js
@@ -7,21 +7,40 @@
 // This is the Splash Scene
 
 class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the construtor.
+   */
   constructor() {
     super({ key: "splashScene" })
   }
 
+  /**
+   * This method is the init.
+   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
+  /**
+   * This method is the preload.
+   */
   preload() {
     console.log("Splash Scene")
   }
 
-  create(data) {}
+  /**
+   * This method is the create.
+   */
+  create(data) {
+    // pass
+  }
 
-  update(time, delta) {}
+  /**
+   * This method is the update.
+   */
+  update(time, delta) {
+    // pass
+  }
 }
 
 export default SplashScene
