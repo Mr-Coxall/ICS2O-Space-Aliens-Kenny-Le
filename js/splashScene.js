a6853afe0702a45a68d81151d24eee9d08b236ab
Patrick Coxall <mr.coxall@mths.ca>
2022-04-24 09:41:46 -0400

    update jsdocs

--- js/splashScene.js
+++ js/splashScene.js
@@ -18,10 +18,30 @@ class SplashScene extends Phaser.Scene {
   }
 
   /**
-   * This method is the init.
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *   before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets.
+   */
+  preload() {
+    console.log("Splash Scene")
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
+    // pass
   }
 
   /**
@@ -29,20 +49,6 @@ class SplashScene extends Phaser.Scene {
    * This method is called once per game step while the scene is running.
    *  @param {number} time - The current time.
    *  @param {number} delta - The delta time in ms since the last frame.
-   */
-  preload() {
-    console.log("Splash Scene")
-  }
-
-  /**
-   * This method is the create.
-   */
-  create(data) {
-    // pass
-  }
-
-  /**
-   * This method is the update.
    */
   update(time, delta) {
     // pass
